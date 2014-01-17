5cc09f48c7151ae182dcd41b616d9c3b6bb272a9
Dmytro Shylovskyi <dimas09@ukr.net>
2014-01-17 18:19:51 +0200

    exit after test pushing to redis channel

--- publish.js
+++ publish.js
@@ -4,9 +4,26 @@ if (config.redis.auth) {
 	redisClient.auth(config.redis.auth);
 }
 
-redisClient.publish(config.redis.channel, JSON.stringify({"toUserId": 101, "message": new Date().getTime()}));
-redisClient.publish(config.redis.channel, JSON.stringify({"toUserId": 102, "message": new Date().getTime()}));
-redisClient.publish(config.redis.channel, JSON.stringify({"toUserId": 103, "message": new Date().getTime()}));
-redisClient.publish(config.redis.channel, JSON.stringify({"toUserId": 104, "message": new Date().getTime()}));
+var status = [false, false, false, false, false, false, false];
+redisClient.publish(config.redis.channel, JSON.stringify({"toUserId": 101, "message": new Date().getTime()}), 
+	function (){ status[0]=true; exit(); });
+redisClient.publish(config.redis.channel, JSON.stringify({"toUserId": 102, "message": new Date().getTime()}), 
+	function (){ status[1]=true; exit(); });
+redisClient.publish(config.redis.channel, JSON.stringify({"toUserId": 103, "message": new Date().getTime()}), 
+	function (){ status[2]=true; exit(); });
+redisClient.publish(config.redis.channel, JSON.stringify({"toUserId": 104, "message": new Date().getTime()}), 
+	function (){ status[3]=true; exit(); });
+redisClient.publish(config.redis.channel, JSON.stringify({"toUserId": 105, "message": new Date().getTime()}), 
+	function (){ status[4]=true; exit(); });
+redisClient.publish(config.redis.channel, JSON.stringify({"toUserId": 106, "message": new Date().getTime()}), 
+	function (){ status[5]=true; exit(); });
+redisClient.publish(config.redis.channel, JSON.stringify({"toUserId": 107, "message": new Date().getTime()}), 
+	function (){ status[6]=true; exit(); });
 
-// process.exit(1);
+function exit() {
+	var exit = false
+	status.forEach(function(value) { exit = value })
+	if (exit) {
+		process.exit(1);
+	}
+}
